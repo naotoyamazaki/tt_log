8f1e330b5e39d7d091c7865844fe8e499ec05560
naotoyamazaki <zakiyama5518@gmail.com>
2024-02-19 15:29:31 +0900

    分析一覧ページ実装

---
+++ app/views/match_infos/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.array! @match_infos, partial: "match_infos/match_info", as: :match_info
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :users, only: %i[new create]
+  resources :match_infos
 end

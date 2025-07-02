9acf2513bf1d0246013ec043d26a5b6f896184be
naotoyamazaki <zakiyama5518@gmail.com>
2025-07-02 22:05:37 +0900

    Add:ActiveJobアダプタをSidekiqに変更、ジョブの作成

---
+++ app/jobs/advice_generation_job.rb
@@ -0,0 +1,16 @@
+# app/jobs/advice_generation_job.rb
+class AdviceGenerationJob < ApplicationJob
+  queue_as :default
+
+  def perform(match_info_id)
+    match_info = MatchInfo.find_by(id: match_info_id)
+    return if match_info.nil? || match_info.advice.present?
+
+    batting_scores = match_info.batting_score_data
+    Rails.logger.info("非同期でChatGPTに送信: #{batting_scores.to_json}")
+    advice = ChatgptService.get_advice(batting_scores.to_json)
+    match_info.update_advice(advice)
+  rescue StandardError => e
+    Rails.logger.error("AdviceGenerationJob Error: #{e.message}")
+  end
+end

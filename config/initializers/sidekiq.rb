23deef57ec1e401d6edc02fdd8fbdd7faafcc16a
naotoyamazaki <zakiyama5518@gmail.com>
2025-07-21 17:04:42 +0900

    sidekiq.rbに本番用のREDIS_URLを設定

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
+redis_url = ENV.fetch('REDIS_URL', 'redis://redis:6379/0')
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: redis_url }
 end

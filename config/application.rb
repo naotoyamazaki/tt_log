d0d1c0f9372029daf43c29f03209d5380f7f328e
naotoyamazaki <zakiyama5518@gmail.com>
2025-08-02 17:33:53 +0900

    Dockerからbuildpackベースのデプロイに切り替え

--- config/application.rb
+++ config/application.rb
@@ -2,29 +2,20 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TtLog
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 7.1
 
     config.active_record.timestamped_migrations = true
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
     config.i18n.default_locale = :ja
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
   end
 end

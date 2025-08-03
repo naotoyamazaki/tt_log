8f3238cea85f919dfaa3272b1184e05a8c238564
naotoyamazaki <zakiyama5518@gmail.com>
2025-08-03 17:18:43 +0900

    SidekiqのRedis SSL検証を修正

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,12 @@
+require "redis"
+require "sidekiq"
+
 redis_url = ENV.fetch('REDIS_URL', 'redis://redis:6379/0')
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
+  config.redis = { url: redis_url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
+  config.redis = { url: redis_url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end

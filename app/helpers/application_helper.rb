db393a06afc1e69eea5c5e5875b9f9fd5921b0e0
naotoyamazaki <zakiyama5518@gmail.com>
2025-08-09 14:06:17 +0900

    Add:得点率表示統一のため小数点以下を非表示設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,13 @@ module ApplicationHelper
     total_score = scores.sum(:score)
     total_lost_score = scores.sum(:lost_score)
     total = total_score + total_lost_score
-    total.positive? ? (total_score.to_f / total * 100).round(2) : 0
+    total.positive? ? (total_score.to_f / total * 100).round : 0
   end
 
   def calculate_batting_score_data(batting_scores)
     data = batting_scores.map do |score|
       total = score.score + score.lost_score
-      rate = total.positive? ? (score.score.to_f / total * 100).round(2) : 0
+      rate = total.positive? ? (score.score.to_f / total * 100).round : 0
       { batting_style: score.batting_style, rate: rate }
     end
 
